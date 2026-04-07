efd8224d62e50c440664c33ff8d24a512a01b413
Alchyr <isithalchyr@gmail.com>
2026-04-07 12:17:14 -0700

    allow setting arbitrary ID

--- Patches/Content/PrefixIdPatch.cs
+++ Patches/Content/PrefixIdPatch.cs
@@ -1,6 +1,8 @@
-using System;
+using System.Collections.Concurrent;
+using System.Reflection;
 using BaseLib.Abstracts;
 using BaseLib.Extensions;
+using BaseLib.Utils.Attributes;
 using HarmonyLib;
 using MegaCrit.Sts2.Core.Models;
 
@@ -9,15 +11,30 @@ namespace BaseLib.Patches.Content;
 [HarmonyPatch(typeof(ModelDb), nameof(ModelDb.GetEntry))]
 public class PrefixIdPatch
 {
+    private static readonly ConcurrentDictionary<Type, string> IdCache = new();
 
     [HarmonyPostfix]
     static string AdjustID(string __result, Type type)
     {
+        if (IdCache.TryGetValue(type, out var cachedId))
+        {
+            return cachedId;
+        }
+        
+        var attr = type.GetCustomAttribute<CustomIDAttribute>();
+        if (attr != null)
+        {
+            IdCache[type] = attr.ID;
+            return attr.ID;
+        }
+        
         if (type.IsAssignableTo(typeof(ICustomModel)))
         {
-            //MainFile.Logger.Info(s);
-            return type.GetPrefix() + __result;
+            IdCache[type] = type.GetPrefix() + __result;
+            return IdCache[type];
         }
+
+        IdCache[type] = __result;
         return __result;
     }
 }
---
+++ Utils/Attributes/CustomIDAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace BaseLib.Utils.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public class CustomIDAttribute(string id) : Attribute
+{
+    public string ID { get; } = id;
+}
